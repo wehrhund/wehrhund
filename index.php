4a293fdd2c998fe694f2dc63fa6d52c97f32a643
Wehrhund <wehrhund@wehrhund.com>
2012-01-16 17:31:09 -0500

    complete rewrite

--- index.php
+++ index.php
@@ -1,9 +1,24 @@
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="style.css" />
-<title>wehrhund</title>
 </head>
+
 <body>
+
+<div class="mask">
+ <div class="header">
 <h1>wehrhund</h1>
+ </div> 
+ <div class="colleft">
+  <div class="col1">
+  </div>
+  <div class="col2">
+  </div> 
+ </div>
+ <div class="footer">
+<font size="-2"><p>&copy; 2012 wehrhund.com</p></font>
+ </div>
+</div>
+
 </body>
 </html>
