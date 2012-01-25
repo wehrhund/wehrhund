bd9473472beea54371b04e6443cd14181721f304
Wehrhund <wehrhund@wehrhund.com>
2012-01-24 19:24:02 -0500

    Autocommit

--- index.php
+++ index.php
@@ -6,6 +6,16 @@
   <meta name="Author" content="wehrhund" />
   <meta name="description" lang="en" content="wehrhund"/>
   <meta name="keywords" lang="en" content="wehrhund" />  
+  <script type="text/javascript">
+   var _gaq = _gaq || [];
+   _gaq.push(['_setAccount', 'UA-XXXXX-X']);
+   _gaq.push(['_trackPageview']);
+   (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+   })();
+  </script>
  </head>
 
  <body>
@@ -17,17 +27,6 @@
    <a href="http://picplz.com/user/wehrhund/" target="_blank"><img src="images/picplz.png" border="0" alt="picplz"></a>
    <a href="skype:wehrhund?add" target="_blank"><img src="images/skype.png" border="0" alt="skype"></a>
   </div>
-
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-try{ 
-var pageTracker = _gat._getTracker("UA-28601269-1");
-pageTracker._trackPageview();
-} catch(err) {} 
-</script>
 
  </body>
  
