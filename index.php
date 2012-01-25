6c65505b369227fc73db416d436a6a514a2ba5b4
Wehrhund <wehrhund@wehrhund.com>
2012-01-24 19:49:21 -0500

    Autocommit

--- index.php
+++ index.php
@@ -23,10 +23,10 @@
 
   <div id="centerbox">
    <img src="images/wehrhund.png"><br><br>
-   <a href="http://twitter.com/wehrhund" target="_blank"><img src="images/twitter.png" border="0" alt="twitter"></a>
-   <a href="http://tumblr.wehrhund.com" target="_blank"><img src="images/tumblr.png" border="0" alt="tumblr."></a>
-   <a href="http://picplz.com/user/wehrhund/" target="_blank"><img src="images/picplz.png" border="0" alt="picplz"></a>
-   <a href="skype:wehrhund?add" target="_blank"><img src="images/skype.png" border="0" alt="skype"></a>
+   <a href="http://twitter.com/wehrhund" target="_blank"><img id="socialink" src="images/twitter.png" border="0" alt="twitter"></a>
+   <a href="http://tumblr.wehrhund.com" target="_blank"><img id="socialink" src="images/tumblr.png" border="0" alt="tumblr."></a>
+   <a href="http://picplz.com/user/wehrhund/" target="_blank"><img id="socialink" src="images/picplz.png" border="0" alt="picplz"></a>
+   <a href="skype:wehrhund?add" target="_blank"><img id="socialink" src="images/skype.png" border="0" alt="skype"></a>
   </div>
 
  </body>
